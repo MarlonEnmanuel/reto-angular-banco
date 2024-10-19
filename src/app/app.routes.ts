28b7f12b89f7d145b1ae58bcc19d8df14b1468e5
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2024-10-18 21:02:26 -0500

    Implement not found component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LayoutComponent,
         children: [
-            
         ]
     },
+    {
+        path: '**',
+        component: NotFoundComponent,
+    }
 ];
