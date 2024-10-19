efcfd73744eb689fde8cace6dfdafcbe2510d233
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2024-10-19 17:19:06 -0500

    F1: implement product list component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../services/products-api.models';
 
 @Component({
     selector: 'app-product-list',
@@ -9,4 +10,6 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+    @Input({ required: true })
+    public products!:Product[];
 }
