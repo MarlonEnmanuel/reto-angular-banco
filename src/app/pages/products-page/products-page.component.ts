6b3624440b72d0e3aa9c732de86fd47dd939bf69
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2024-10-19 18:13:57 -0500

    F2: Implement products search

--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -1,18 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { ProductsApiService } from '../../services/products-api.service';
 import { Product } from '../../services/products-api.models';
 import { ProductListComponent } from "../../components/product-list/product-list.component";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-products-page',
     standalone: true,
-    imports: [ProductListComponent],
+    imports: [
+        ProductListComponent,
+        ReactiveFormsModule,
+    ],
     templateUrl: './products-page.component.html',
     styleUrl: './products-page.component.scss'
 })
 export class ProductsPageComponent {
 
-    public products = signal<Product[]>([]);
+    private products = signal<Product[]>([]);
+
+    public search = signal<string>('');
+
+    public filteredProducts = computed(() => {
+        const products = this.products();
+        const search = this.search().trim();
+        return products.filter((product) =>
+            product.name.toLowerCase().includes(search) ||
+            product.description.toLowerCase().includes(search) ||
+            product.date_release.toLowerCase().includes(search) ||
+            product.date_revision.toLowerCase().includes(search)
+        );
+    });
+
+    public searchInput = new FormControl<string>('', { nonNullable: true });
     
     constructor(
         private productsApiService: ProductsApiService
