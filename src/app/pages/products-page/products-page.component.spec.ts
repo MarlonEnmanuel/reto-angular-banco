b77e84853bfa59514c80b3f4ad061336a5ff9c21
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2024-10-21 17:33:55 -0500

    Add product page tests

--- src/app/pages/products-page/products-page.component.spec.ts
+++ src/app/pages/products-page/products-page.component.spec.ts
@@ -28,14 +28,24 @@ describe('ProductsPageComponent', () => {
             ]
         })
         .compileComponents();
-
-        apiMocker.mockGetProducts();
+        
         fixture = TestBed.createComponent(ProductsPageComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
     it('should create', () => {
+        apiMocker.mockGetProducts([]);
+        fixture.detectChanges();
         expect(component).toBeTruthy();
     });
+
+    it('should show product list', () => {
+        apiMocker.mockGetProducts([
+            { id: '001', name: 'Product 1', description: 'Description 1', logo: 'img', date_release: '2021-01-01', date_revision: '2021-01-01' },
+        ]);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelector('.product-list')).toBeTruthy();
+        expect(compiled.querySelector('.product-list table')).toBeTruthy();
+    })
 });
